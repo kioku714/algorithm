ac63930df4b4a89cae7a934242b63e28bb3da273
유미령 <kioku714@gmail.com>
2018-03-15 19:24:53 +0900

    피보나치 수

---
+++ src/Fibonacci.java
@@ -0,0 +1,20 @@
+public class Fibonacci {
+    public long fibonacci(int num) {
+        long[] numbers = new long[num + 1];
+        numbers[0] = 0;
+        numbers[1] = 1;
+
+        for(int i = 2; i <= num; i++) {
+            numbers[i] = numbers[i - 1] + numbers[i - 2];
+        }
+
+        return numbers[num];
+    }
+
+    // 아래는 테스트로 출력해 보기 위한 코드입니다.
+    public static void main(String[] args) {
+        Fibonacci c = new Fibonacci();
+        int testCase = 3;
+        System.out.println(c.fibonacci(testCase));
+    }
+}
